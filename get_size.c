744064cc2bd697578e4786d2fa53231d891f18f3
major999 <momohamed2157@gmail.com>
2023-04-26 03:10:31 +0000

    my edit

--- get_size.c
+++ get_size.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
- * Return: Precision.
+ * get_size - Caqwdwqdqwdqwdqwdwqdnt.
+ * @format: Foqwdwqdqwdqwdqwdqwments.
+ * @i: qwdwqdqwdqwdwqdwqd.
+ * Return: nothinghfoisajdioasjd.
  */
 
 int get_size(const char *format, int *i)
@@ -14,14 +13,30 @@ int get_size(const char *format, int *i)
 	int size = 0;
 
 	if (format[curr_i] == 'l')
+
+	{
 		size = 2;
+	}
+
 	else if (format[curr_i] == 'h')
+
+	{
 		size = 1;
+	}
+
 
 	if (size == 0)
+
+	{
 		*i = curr_i - 1;
+	}
+
 	else
+
+	{
 		*i = curr_i;
+	}
+
 
 	return (size);
 }
--- get_width.c
+++ get_width.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: width.
+ * get_width - Caqwdqwdqwdqwting
+ * @format: Fqwdqwdqwqwdqwments.
+ * @i: List of arqwdqwwqdrinted.
+ * @list: lisqwdqwdt of aب90ioqjwdioujwqidojts.
+ * Return: widqwdqwdqwdqwdqwd
  */
 int get_width(const char *format, int *i, va_list list)
 {
@@ -14,20 +13,29 @@ int get_width(const char *format, int *i, va_list list)
 	int width = 0;
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+
 	{
 		if (is_digit(format[curr_i]))
+
 		{
 			width *= 10;
 			width += format[curr_i] - '0';
 		}
+
 		else if (format[curr_i] == '*')
+
 		{
 			curr_i++;
 			width = va_arg(list, int);
 			break;
 		}
+
 		else
+
+		{
 			break;
+		}
+
 	}
 
 	*i = curr_i - 1;
